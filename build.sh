b04d200881581775d46797617f9c97aa9de783f0
Soichi Hayashi <hayashis@iu.edu>
2018-07-31 15:47:42 +0000

    updated to mrtrix3 added capability to open track.tck

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-docker build -t soichih/vncserver-mrview . && docker push soichih/vncserver-mrview
+set -e
+
+tag=3
+
+docker build -t soichih/vncserver-mrview .
+docker tag soichih/vncserver-mrview soichih/vncserver-mrview:$tag
+docker push soichih/vncserver-mrview #latest
+docker push soichih/vncserver-mrview:$tag
